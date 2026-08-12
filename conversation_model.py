a8b05c1ab3d45cb56be2e4c98ec4392256351f8b
Navdeep <rajnavdeep8@gmail.com>
2026-08-12 16:08:06 +0530

    Coversation model created

---
+++ conversation_model.py
@@ -0,0 +1,23 @@
+import requests
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain.messages import HumanMessage, AIMessage, SystemMessage
+from langchain.tools import tool
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+model = init_chat_model(
+    "google_genai:gemini-3.1-flash-lite",
+    temperature=0.1
+)
+
+conversation=[
+    SystemMessage('You are a powerful agent'),
+    HumanMessage('What is Python?'),
+    AIMessage('Python is a interpreter language'),
+    HumanMessage('When was it released?')
+    ]
+response=model.invoke(conversation)
+print(response)
+print(response.content)
