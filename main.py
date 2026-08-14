08dde50704bbed8b76f6a058e97f2a6dc4860123
Navdeep <rajnavdeep8@gmail.com>
2026-08-14 13:51:43 +0530

    Main file edited

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import requests
+from dtaclasses import dataclass
+from langchsin.chat_models import init_chat_model
 from dotenv import load_dotenv
 
 from langchain.agents import create_agent
-from langchain.tools import tool
+from langchain.tools import tool, ToolRuntime
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.chechpoint.memory import InMemorysaver
+
 
 
 load_dotenv()
+
+@dataclass
+class Context:
+    user_id:str
+
+@dataclass
+class ResponseFormat:
+    summary:str
+    temperature_celcius:float
+    temperature_farenheit:float
+    humidity:float
+
 
 
 @tool(
@@ -19,6 +35,11 @@ def weather(city: str):
         f"https://wttr.in/{city}?format=j1"
     )
     return response.json()
+
+@tool('locate_user',description="Look up a user's city based on the context")
+def locate_user(runtime:toolRuntime[Context]):
+    match runtime.context.user_id:
+        case"ABC123" return 'Vietnam'
 
 
 llm = ChatGoogleGenerativeAI(
