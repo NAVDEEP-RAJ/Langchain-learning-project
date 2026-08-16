e086c86c37348f191fb41603face30977565f89d
Allwin Ranjithkumar <allwinranjithkumar@gmail.com>
2026-08-17 01:52:03 +0530

    New file

--- main.py
+++ main.py
@@ -1,71 +1,14 @@
-import requests
-from dtaclasses import dataclass
-from langchsin.chat_models import init_chat_model
 from dotenv import load_dotenv
-
-from langchain.agents import create_agent
-from langchain.tools import tool, ToolRuntime
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.chechpoint.memory import InMemorysaver
-
-
+from langchain.chat_models import init_chat_model
 
 load_dotenv()
 
-@dataclass
-class Context:
-    user_id:str
-
-@dataclass
-class ResponseFormat:
-    summary:str
-    temperature_celcius:float
-    temperature_farenheit:float
-    humidity:float
-
-
-
-@tool(
-    "get_weather",
-    description="Return the weather information for a given city",
-    return_direct=False
-)
-def weather(city: str):
-    response = requests.get(
-        f"https://wttr.in/{city}?format=j1"
-    )
-    return response.json()
-
-@tool('locate_user',description="Look up a user's city based on the context")
-def locate_user(runtime:toolRuntime[Context]):
-    match runtime.context.user_id:
-        case"ABC123" return 'Vietnam'
-
-
-llm = ChatGoogleGenerativeAI(
-    model="gemini-3.5-flash"
+model = init_chat_model(
+    "google_genai:gemini-3.1-flash-lite",
+    temperature=0.7
 )
 
+response = model.stream("What is LangChain?")
 
-agent = create_agent(
-    model=llm,
-    tools=[weather],
-    system_prompt="You are a helpful assistant who always cracks jokes"
-)
-
-
-response = agent.invoke(
-    {
-        "messages": [
-            {
-                "role": "user",
-                "content": "What is the weather like in Mumbai?"
-            }
-        ]
-    }
-)
-
-
-print(response)
-print(response["messages"][-1].content)
-
+for chunk in response:
+    print(chunk.text, end="")
